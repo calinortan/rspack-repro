570033c1d7c56ef2e950b491c8977609ae8d5748
Calin Ortan <calinortan@gmail.com>
2024-04-05 15:04:55 +0300

    bug-repro

--- src/render.js
+++ src/render.js
@@ -1,8 +1,21 @@
-import './render.css'
+import "./render.css";
 
 export function render() {
-    const el = document.createElement('div')
-    el.classList.add('text')
-    document.getElementsByTagName('body')[0].appendChild(el)
-    el.innerHTML = 'hello, world'
+  const xhr = {
+    responseType: "",
+    responseText: "hello",
+    response: "world",
+  };
+
+  const x =
+    (xhr.responseType || "text") !== "text" || xhr.responseText !== "string"
+      ? "hello"
+      : "world";
+
+  console.log(x);
+
+  const el = document.createElement("div");
+  el.classList.add("text");
+  document.getElementsByTagName("body")[0].appendChild(el);
+  el.innerHTML = x;
 }
